636bc2ab8cebbbd1ca5fa3bf0ce9646a35d74743
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-12 00:19:04 +0530

    add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
     <div className='flex justify-center items-center h-screen text-red-500'>
-      <Button>Button</Button>{" "}
+      <UserButton user={user} />
     </div>
   );
 }
